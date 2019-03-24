eed61da15c9a935ade4818c8bb975a9bfd3866ce
young <yangping0607@163.com>
2019-03-24 02:00:29 +0000

    home,about,help of Static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>@yield('title','Weibo App') -- 平轩博客</title>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,8 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//主页
+Route::get('/','StaticPagesController@home');
+//帮助页
+Route::get('help','StaticPagesController@help');
+//关于页
+Route::get('about','StaticPagesController@about');
