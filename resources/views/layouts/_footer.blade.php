3e6ef3df65f124745a33909604487d38a7bb1cce
young <yangping0607@163.com>
2019-03-24 10:10:12 +0000

    header,footer seperate from default

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="branch-icon" src="https://iocaffcdn.phphub.org/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" alt="底部logo">
+  <a href="http://www.yangp67.com" target="_blank">
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about">关于</a>
+  </div>
+</footer>
