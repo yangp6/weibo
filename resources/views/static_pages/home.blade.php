a93629047195a39b331adc9f46cd1adfd917ce25
young <yangping0607@163.com>
2019-03-24 05:16:37 +0000

    Init css use bootstrap

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -2,5 +2,16 @@
 @section('title','主页')
 
 @section('content')
-  <h1>主页</h1>
+  <div class="jumbotron">
+    <h1>Welcome to My Weibo</h1>
+    <p class="lead">
+      现在所看到的是 <a href="http://www.yangp067.com">平轩博客</a> 的微博项目主页
+    </p>
+    <p>
+      一切，又从这里开始
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">现在注册</a>
+    </p>
+  </div>
 @stop
