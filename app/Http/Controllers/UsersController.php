0b2485c8a7fab4d0322934189593169a5e72e2d8
young <yangping0607@163.com>
2019-03-24 10:43:06 +0000

    static pages of sugnup

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+/**
+ * 用户控制器
+ */
+class UsersController extends Controller
+{
+    //注册页面
+    public function create()
+    {
+        return view('users.create');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,6 @@ Route::get('/','StaticPagesController@home')->name('home');
 Route::get('help','StaticPagesController@help')->name('help');
 //关于页
 Route::get('about','StaticPagesController@about')->name('about');
+
+//注册页面
+Route::get('signup','UsersController@create')->name('signup');
