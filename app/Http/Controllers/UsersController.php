2deeee599b11535b4ebc76c9a0954bc4d6341e8c
young <yangping0607@163.com>
2019-03-25 13:40:03 +0000

    注册表单构建，增加 csrf 验证

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -14,10 +14,23 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+    //注册操作
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'name'=>'required|max:50',                      //必填， 最大长度50
+            'email'=>'required|email|unique:users|max:255', //必填，邮箱格式，唯一性，最大长度255
+            'password'=>'required|confirmed|min:6',         //必填，两次密码一致，最小长度6位
+        ]);
+        return;
+    }
 
     //展示用户个人信息
     public function show(User $user)
     {
         return view('users.show',compact('user'));
     }
+
+
+
 }
