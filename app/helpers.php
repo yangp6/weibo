8c7bfcb21ebe6a3b5d23e1f4aed0974179269256
young <yangping0607@163.com>
2019-03-25 14:59:16 +0000

    更新数据库配置

---
+++ app/helpers.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 根据应用不同的运行环境来指定数据库配置信息
+ */
+function get_db_config()
+{
+    if (getenv('IS_IN_HEROKU')) {
+        //postgreSQL
+        $url = parse_url(getenv("DATABASE_URL"));
+
+        return $db_config = [
+            'connection' => 'pgsql',
+            'host' => $url["host"],
+            'database'  => substr($url["path"], 1),
+            'username'  => $url["user"],
+            'password'  => $url["pass"],
+        ];
+    } else {
+        //MySQL
+        return $db_config = [
+            'connection' => env('DB_CONNECTION', 'mysql'),
+            'host' => env('DB_HOST', 'localhost'),
+            'database'  => env('DB_DATABASE', 'forge'),
+            'username'  => env('DB_USERNAME', 'forge'),
+            'password'  => env('DB_PASSWORD', ''),
+        ];
+    }
+}
